17bcc623c83cc602cfd0b125af7a1fb9e570b33d
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-08-05 22:23:45 +0300

    Insert employee

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function addEmployee() {
-    document.getElementById("table-list").innerHTML += "<tr><th scope='row'>IMG</td><td>John</td><td>Doe</td><td>johnd@doe.com</td><td>M</td><td>28 April 2000</td></tr>";
+    let firstName = document.querySelector('#first-name').value;
+    let lastName = document.querySelector('#last-name').value;
+    let email = document.querySelector('#email').value;
+    let gender = document.querySelector('#gender').value;
+    let dob = document.querySelector('#birthday').value;
+    let img = document.querySelector('#avatar');
+    img = img.files.item(0).name;
+
+    document.getElementById("table-list").innerHTML += "<tr><td><img src='images/" + img + "' height='50'></td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + email + "</td><td>" + gender + "</td><td>" + dob + "</td></tr>";
 }
