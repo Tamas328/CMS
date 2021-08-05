ceed3fd769428a3d4d6efd6425500062f3d121a6
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-08-05 20:49:15 +0300

    Form design

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-document.getElementById("table-list").innerHTML = "<tr><th scope='row'>IMG</td><td>John</td><td>Doe</td><td>johnd@doe.com</td><td>M</td><td>28 April 2000</td></tr>";
+function addEmployee() {
+    document.getElementById("table-list").innerHTML += "<tr><th scope='row'>IMG</td><td>John</td><td>Doe</td><td>johnd@doe.com</td><td>M</td><td>28 April 2000</td></tr>";
+}
