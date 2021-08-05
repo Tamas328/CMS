5ca007860035d3907bca2255d5fe208362a52ffd
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-08-05 22:39:18 +0300

    Date fomat

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
 function addEmployee() {
     let firstName = document.querySelector('#first-name').value;
     let lastName = document.querySelector('#last-name').value;
@@ -6,6 +21,17 @@ function addEmployee() {
     let dob = document.querySelector('#birthday').value;
     let img = document.querySelector('#avatar');
     img = img.files.item(0).name;
+    let birthday = getDOB(dob);
 
-    document.getElementById("table-list").innerHTML += "<tr><td><img src='images/" + img + "' height='50'></td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + email + "</td><td>" + gender + "</td><td>" + dob + "</td></tr>";
+    document.getElementById("table-list").innerHTML += "<tr><td><img src='images/" + img + "' height='50'></td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + email + "</td><td>" + gender + "</td><td>" + birthday + "</td></tr>";
+}
+
+function getDOB(dob) {
+    const d = new Date(dob);
+    const year = d.getFullYear();
+    const date = d.getDate();
+    const mIndex = d.getMonth();
+    const month = months[mIndex];
+    
+    return date + " " + month + " " + year;
 }
