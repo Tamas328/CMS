0134a20b1f80626f34b492a73f9bc2b4f7712a52
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-08-08 19:54:20 +0300

    Search bar - first name + last name

--- index.js
+++ index.js
@@ -13,32 +13,31 @@ const months = [
     'December'
 ];
 
-for(var i = 0; i < localStorage.length; ++i) {
-    var storedData = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    document.getElementById("table-list").innerHTML += "<tr>\
-        <td><img src='images/" + storedData[0] + "' height='50'></td>\
-        <td>" + storedData[1] + "</td>\
-        <td>" + storedData[2] + "</td>\
-        <td>" + storedData[3] + "</td>\
-        <td>" + storedData[4] + "</td>\
-        <td>" + storedData[5] + "</td>\
-        <td ><svg xmlns='http://www.w3.org/2000/svg' onclick='deleteRow(this)' width='24' height='24' fill='currentColor' class='bi bi-person-dash-fill deleteRow' viewBox='0 0 16 16'>\
-            <path fill-rule='evenodd' d='M11 7.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z'/><path d='M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'/></svg>\
-        </td>\
-    </tr>";
+loadTable();
+
+function loadTable() {
+    for(var i = 0; i < localStorage.length; ++i) {
+        var storedData = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        document.getElementById("table-list").innerHTML += "<tr>\
+            <td><img src='images/" + storedData[0] + "' height='50'></td>\
+            <td>" + storedData[1] + "</td>\
+            <td>" + storedData[2] + "</td>\
+            <td>" + storedData[3] + "</td>\
+            <td>" + storedData[4] + "</td>\
+            <td>" + storedData[5] + "</td>\
+            <td ><svg xmlns='http://www.w3.org/2000/svg' onclick='deleteRow(this)' width='24' height='24' fill='currentColor' class='bi bi-person-dash-fill deleteRow' viewBox='0 0 16 16'>\
+                <path fill-rule='evenodd' d='M11 7.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z'/><path d='M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'/></svg>\
+            </td>\
+        </tr>";
+    }
 }
+
 
 function addEmployee() {
     const data = [];
     let firstName = document.querySelector('#first-name').value;
     let lastName = document.querySelector('#last-name').value;
     let email = document.querySelector('#email').value;
-
-    const regx = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-    if(!regx.test(email)) {
-        alert("Invalid email format.");
-        return; 
-    }
 
     for(var i = 0; i < localStorage.length; ++i) {
         var storedData = JSON.parse(localStorage.getItem(localStorage.key(i)));
@@ -55,6 +54,12 @@ function addEmployee() {
     if(firstName == "" || lastName == "" || email == "" || gender == "Not specified" || !dob) {
         alert("Please fill out all fields.");
         return;
+    }
+
+    const regx = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    if(!regx.test(email)) {
+        alert("Invalid email format.");
+        return; 
     }
 
     if(img.files.length == 0) {
@@ -78,9 +83,9 @@ function addEmployee() {
             <path fill-rule='evenodd' d='M11 7.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z'/><path d='M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'/></svg>\
         </td>\
     </tr>";
-    document.getElementById("employee").reset();
 
     localStorage.setItem(email, JSON.stringify(data));
+    document.getElementById("employee").reset();
 }
 
 function getDOB(dob) {
@@ -97,4 +102,23 @@ function deleteRow(r) {
     var i = r.parentNode.parentNode.rowIndex;
     localStorage.removeItem(localStorage.key(i - 1));
     document.getElementById("emp-table").deleteRow(i);
+}
+
+function search_filter() {
+    var input = document.getElementById("search-bar");
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("emp-table");
+    var tr = table.getElementsByTagName("tr");
+    for(var i = 0; i < tr.length; ++i) {
+        var firstName = tr[i].getElementsByTagName("td")[1];
+        var lastName = tr[i].getElementsByTagName("td")[2];
+        if(firstName && lastName) {
+            var txtValue = firstName.textContent + " " + lastName.textContent || firstName.innerText + " " + lastName.innerText;
+            if(txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 }
